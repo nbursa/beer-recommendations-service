43c9a8494a7cff7f441a8f7cba8b817c4f1c9207
Nenad <nbursa@gmail.com>
2024-08-22 17:05:10 +0200

    Improve details page layout

--- src/pages/BeerDetailsPage.tsx
+++ src/pages/BeerDetailsPage.tsx
@@ -17,21 +17,32 @@ function BeerDetailsPage() {
 
   return (
     <div className="p-4 w-screen h-screen overflow-y-auto">
-      <h1 className="text-4xl font-bold text-center">{beer.name}</h1>
-      <div className="max-w-screen-md mx-auto mt-4">
+      <div className="max-w-xl mx-auto p-4 flex flex-col gap-4 rounded">
         <img
           src={beer.image}
           alt={beer.name}
-          className="w-full h-60 object-cover"
+          className="w-full max-w-52 max-h-52 mx-auto object-contain rounded"
         />
-        <p>Price: {beer.price}</p>
-        <p>
-          Rating: {beer.rating.average.toFixed(2)} ({beer.rating.reviews}{" "}
-          reviews)
-        </p>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
-          Buy
-        </button>
+        <div className="flex flex-col justify-between gap-4">
+          <h2 className="text-3xl text-center font-bold">{beer.name}</h2>
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <span>Price: </span> <span className="text-lg">{beer.price}</span>
+            </div>
+            <div className="text-sm">
+              <span>Rating: </span>
+              <span className="text-lg">{beer.rating.average.toFixed(2)} </span>
+              <div className="text-xs italic">
+                ({beer.rating.reviews} reviews)
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <button className="w-full border bg-yellow-500 text-black hover:bg-yellow-400 hover:border-none hover:scale-105 transition duration-150 ease-in-out p-2 rounded">
+              Buy
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
