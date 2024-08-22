84e3b61ee115a1b529740b0774e581d36c15df52
Nenad <nbursa@gmail.com>
2024-08-22 11:16:52 +0200

    Implement navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.tsx";
 import BeerDetailsPage from "./pages/BeerDetailsPage.tsx";
 import ManagementPage from "./pages/ManagementPage.tsx";
+import Navigation from "./components/Navigation.tsx";
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/beer/:id" element={<BeerDetailsPage />} />
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,23 @@
+import { Link, useLocation } from "react-router-dom";
+
+function Navigation() {
+  const location = useLocation();
+
+  const getLinkClasses = (path: string) =>
+    `p-2 text-white ${location.pathname === path ? "text-bold" : ""}`;
+
+  return (
+    <nav className="p-4">
+      <div className="flex justify-center space-x-4">
+        <Link to="/" className={getLinkClasses("/")}>
+          Home
+        </Link>
+        <Link to="/management" className={getLinkClasses("/management")}>
+          Management
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
